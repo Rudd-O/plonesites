7b80f42f7532fdb04feb6c6f35a5d3532563c0dd
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2014-09-23 22:58:30 -0700

    Fix language issues with portal

--- src/ruddocom.policy/ruddocom/policy/setupHandlers.py
+++ src/ruddocom.policy/ruddocom/policy/setupHandlers.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from Products.CMFCore.utils import getToolByName
 from Products.PortalTransforms.Transform import make_config_persistent
 
@@ -40,7 +42,15 @@ def setupPortalTransforms(context):
 
 
 @only_when_I_run
-def applyLanguageToContent(context):
+def createContent(context):
     l = context.getSite()
+    if "en" not in l:
+        l.invokeFactory("Folder", "en")
+    if "es" not in l:
+        l.invokeFactory("Folder", "es")
+    l['en'].setTitle(u"Rudd-O.com in English")
+    l['es'].setTitle(u"Rudd-O.com en español")
+    l['en'].setDescription(u"Linux, free software, voluntaryism and cypherpunk.  Established 1999.")
+    l['es'].setDescription(u"Linux, software libre, voluntarismo y cypherpunk.  Desde 1999.")
     l['en'].setLanguage('en')
     l['es'].setLanguage('es')
