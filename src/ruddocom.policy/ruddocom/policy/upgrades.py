3f11cc4ef32758dbedaf9516bc623a187267d1fa
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2015-07-20 23:53:14 -0700

    Add cookie properties to portal, take 2

--- src/ruddocom.policy/ruddocom/policy/upgrades.py
+++ src/ruddocom.policy/ruddocom/policy/upgrades.py
@@ -4,6 +4,3 @@ default_profile = 'profile-ruddocom.policy:default'
 
 def upgrade_1_to_2(context):
     context.runImportStepFromProfile(default_profile, 'propertiestool')
-
-def upgrade_2_to_3(context):
-    context.runImportStepFromProfile(default_profile, 'propertiestool')
