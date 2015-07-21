ddcd1061c766dd6e70e3e8b780f2ccf8c2710732
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2015-07-21 00:12:32 -0700

    Add cookie properties to portal, take 6

--- src/ruddocom.policy/ruddocom/policy/upgrades.py
+++ src/ruddocom.policy/ruddocom/policy/upgrades.py
@@ -6,7 +6,4 @@ def upgrade_1_to_2(context):
     context.runImportStepFromProfile(default_profile, 'propertiestool')
 
 def upgrade_2_to_3(context):
-    l = context.getSite().acl_users.session
-    l.timeout = 604800
-    l.cookie_lifetime = 7
-    l.secure = True
+    context.runImportStepFromProfile(default_profile, 'ruddocom-cookiesettings')
