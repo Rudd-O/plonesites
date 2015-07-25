b487f13f0a4ef61f3d9784d2b42a77905ffb1f2e
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2015-07-25 15:23:27 -0700

    specify that stylesheet override is exclusively for screen

--- src/ruddocom.policy/ruddocom/policy/upgrades.py
+++ src/ruddocom.policy/ruddocom/policy/upgrades.py
@@ -10,3 +10,6 @@ def upgrade_2_to_3(context):
 
 def upgrade_3_to_4(context):
     context.runImportStepFromProfile(default_profile, 'cssregistry')
+
+def upgrade_4_to_5(context):
+    context.runImportStepFromProfile(default_profile, 'cssregistry')
