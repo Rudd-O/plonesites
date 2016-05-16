0e81f319466174743fb17d18fb3103570d4e09fe
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2016-05-16 14:19:07 +0000

    try to fix dependencies again

--- src/ruddocom.policy/setup.py
+++ src/ruddocom.policy/setup.py
@@ -23,6 +23,7 @@ setup(name='ruddocom.policy',
       namespace_packages=['ruddocom'],
       include_package_data=True,
       zip_safe=False,
+      # Keep me in sync with metadata.xml
       install_requires=[
           'setuptools',
           'Plone',
