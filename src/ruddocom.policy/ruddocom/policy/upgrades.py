d21a06189eba0da4daf072c164794e356b388f9c
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2016-05-16 05:01:23 +0000

    kill the upgrades.zcml

--- src/ruddocom.policy/ruddocom/policy/upgrades.py
+++
@@ -1,6 +0,0 @@
-from Products.CMFCore.utils import getToolByName
-
-default_profile = 'profile-ruddocom.policy:default'
-
-def upgrade_to_8(context):
-    context.runImportStepFromProfile(default_profile, 'registry')
