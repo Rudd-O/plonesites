9b7d353f249b4e52e9502785e6ea367746355449
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2016-05-26 06:17:31 +0000

    patch catalog search in sitemap

--- src/plone.app.layout/plone/app/layout/sitemap/sitemap.py
+++ src/plone.app.layout/plone/app/layout/sitemap/sitemap.py
@@ -56,7 +56,7 @@ class SiteMapView(BrowserView):
 
         query['is_default_page'] = True
         default_page_modified = OOBTree()
-        for item in catalog.searchResults(query, Language='all'):
+        for item in catalog.searchResults(query):
             key = item.getURL().rsplit('/', 1)[0]
             value = (item.modified.micros(), item.modified.ISO8601())
             default_page_modified[key] = value
@@ -80,7 +80,7 @@ class SiteMapView(BrowserView):
             }
 
         query['is_default_page'] = False
-        for item in catalog.searchResults(query, Language='all'):
+        for item in catalog.searchResults(query):
             loc = item.getURL()
             date = item.modified
             # Comparison must be on GMT value
