e30caed198a225df82fb17a65c92a9905da63b73
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2016-09-04 01:33:33 +0000

    add transaction note

--- src/ruddocom.policy/ruddocom/policy/upgrader_embedded.py
+++ src/ruddocom.policy/ruddocom/policy/upgrader_embedded.py
@@ -28,18 +28,31 @@ for p in productstoupgrade:
     if p not in installed:
         raise Exception("Product %s is not installed in any site" % p)
 
+changed = ""
+
 for a in (s for s in app.values() if isinstance(s, PloneSite)):
     ip = dict((x['id'], x) for x in a.portal_quickinstaller.listInstalledProducts())
     for i, data in ip.items():
         if i not in productstoupgrade: continue
         ui = a.portal_quickinstaller.upgradeInfo(i)
-        if ui['installedVersion'] != ui['newVersion']:
+        installedVersion, newVersion = ui['installedVersion'], ui['newVersion']
+        if installedVersion != newVersion:
             for k, v in ui.items():
                 print "in: %s: before: %s: %s=%s" % (a.__name__, i, k, v)
             a.portal_quickinstaller.upgradeProduct(i)
             uia = a.portal_quickinstaller.upgradeInfo(i)
             for k, v in uia.items():
                 print "in: %s: after:  %s: %s=%s" % (a.__name__, i, k, v)
+            if changed == "":
+                changed = "Upgrade: "
+            else:
+                changed = changed + ", "
+            changed = changed + "%s from %s to %s" % (
+                a.__name__, installedVersion, newVersion
+            )
 
 if commit:
-    transaction.commit()
+    if changed:
+        t = transaction.get()
+        t.note(changed)
+        t.commit()
