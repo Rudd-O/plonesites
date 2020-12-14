c574fbd8016067b22adda884d74680cbcc5b0f29
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2020-12-14 08:15:27 +0000

    Add find bad oids.

---
+++ scripts/find_bad_oids.py
@@ -0,0 +1,20 @@
+from ZODB.POSException import POSKeyError
+def error_finder(folder, exception=POSKeyError, stop_on_first=None):
+    """ start at the given folderish object.
+    If stop_on_first is true, quit after one exception;
+    otherwise, keep going through the whole tree."""
+    for id, next_item in folder.objectItems():
+        try:
+            next_item.getId()
+        except exception:
+            print `exception`, "in folder",
+            print  '/'.join(folder.getPhysicalPath()),
+            print "at id:", id
+            if stop_on_first:
+                raise "done"   # hack to break out of recursion
+        else:
+            # no error, recurse if it's objectManagerish
+            if hasattr(next_item.aq_base, 'objectItems'):
+                error_finder(next_item, exception, stop_on_first)
+
+error_finder(app.Engr, stop_on_first=False)
