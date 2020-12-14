57d3751b7f7352f132f140b98369ba88c6a21007
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2020-12-14 08:38:59 +0000

    Purge versions (only dry run).

---
+++ scripts/purge_versions.py
@@ -0,0 +1,16 @@
+from DateTime import DateTime
+from Products.CMFCore.utils import getToolByName
+from Products.CMFEditions.utilities import dereference
+
+
+policy = getToolByName(self.context, 'portal_purgepolicy')
+catalog = getToolByName(self.context, 'portal_catalog')
+
+for count, brain in enumerate(catalog()):
+    obj = brain.getObject()
+
+    # only purge old content
+    if obj.created() < (DateTime() - 30):
+        obj, history_id = dereference(obj)
+        # policy.beforeSaveHook(history_id, obj)
+        print('purged object ' + obj.absolute_url_path())
