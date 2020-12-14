c630c555790d403cdb65c30809b679f1fdf67396
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2020-12-14 08:42:07 +0000

    Purge versions (only dry run).

--- scripts/purge_versions.py
+++ scripts/purge_versions.py
@@ -2,7 +2,8 @@ from DateTime import DateTime
 from Products.CMFCore.utils import getToolByName
 from Products.CMFEditions.utilities import dereference
 
-self = app[sys.argv[-1]]
+self = app[sys.argv[-1]].aq_base
+assert 0, self
 
 policy = getToolByName(self.context, 'portal_purgepolicy')
 catalog = getToolByName(self.context, 'portal_catalog')
