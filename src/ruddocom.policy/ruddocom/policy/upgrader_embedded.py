a998f0736cca2f9150d594e6c1246522d8f64947
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2020-12-16 01:53:19 +0000

    Upgrader gains the ability to install multiple packages.

--- src/ruddocom.policy/ruddocom/policy/upgrader_embedded.py
+++ src/ruddocom.policy/ruddocom/policy/upgrader_embedded.py
@@ -18,18 +18,27 @@ if not productstoupgrade:
         "the product(s) to upgrade must " "be specified after the upgrade subcommand"
     )
 
-if len(productstoupgrade) != 2:
-    raise Exception("the product to upgrade must follow the name (ID) of the site")
+if len(productstoupgrade) < 2:
+    raise Exception("the product(s) to upgrade must follow the name (ID) of the site")
 
-siteid, productid = productstoupgrade
+siteid = productstoupgrade[0]
+productstoupgrade = productstoupgrade[1:]
+
+changes = []
 
 qi = get_installer(app[siteid])  # pylint:disable=invalid-name,used-before-assignment
-qi.upgrade_product(productid)
+
+for productid in productstoupgrade:
+    if not qi.is_product_installed(productid):
+        changes.append("Product %s successfully upgraded on %s." % (productid, siteid))
+        qi.install_product(productid)
+    else:
+        changes.append("Product %s successfully installed on %s." % (productid, siteid))
+        qi.upgrade_product(productid)
 
 if commit:
     t = transaction.get()
-    t.note("Upgraded %s on %s" % (productid, siteid))
     t.commit()
-    print("Product %s successfully upgraded on %s." % (productid, siteid))
-else:
-    print("Product %s upgraded in simulation mode on %s."% (productid, siteid))
+    print()
+for change in changes:
+    print(change + " (Simulated.)" if not commit else "")
