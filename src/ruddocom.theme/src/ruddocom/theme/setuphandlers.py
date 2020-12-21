0039c05236b264ef90191145e3e3cf5d3c959dc1
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2020-12-21 10:32:28 +0000

    Add new editor styles.

--- src/ruddocom.theme/src/ruddocom/theme/setuphandlers.py
+++ src/ruddocom.theme/src/ruddocom/theme/setuphandlers.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 import logging
+from six import StringIO
 
+# Unused:
+# from plone.cachepurging.utils import getPathsToPurge
 from Products.CMFPlone.interfaces import INonInstallable
+from plone.cachepurging.interfaces import ICachePurgingSettings
+from plone.cachepurging.interfaces import IPurger
+from plone.cachepurging.utils import getURLsToPurge
+from plone.cachepurging.utils import isCachePurgingEnabled
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
 from zope.interface import implementer
 
 
@@ -40,3 +49,34 @@ def redo_registry(portal_setup):
         "ruddocom.theme:default", "plone.app.registry"
     )
     logger("Done setting up registry properties.")
+
+
+def purge_urls(unused_portal_setup, urls):
+    if not isCachePurgingEnabled():
+        logger("Not purging anything because frontend caching is not enabled.")
+        return 'Caching not enabled'
+    logger("Purging URLs.")
+    purger = getUtility(IPurger)
+    registry = getUtility(IRegistry)
+    settings = registry.forInterface(ICachePurgingSettings)
+    for path in urls:
+        if not settings.cachingProxies: continue
+        for url in getURLsToPurge(path, settings.cachingProxies):
+            status, xcache, xerror = purger.purgeSync(url)
+            logger("Purged %s — Status: %s — X-Cache: %s — Error: %s", url, status, xcache, xerror)
+    logger("Done purging URLs.")
+
+
+def redo_registry_and_purge_certain_urls(portal_setup):
+    try:
+        if portal_setup.readDataFile("ruddocom.theme.txt") is None:
+            return
+    except AttributeError:
+        logger("Called with %s, continuing since it appears we do not need a marker file.", portal_setup)
+    redo_registry(portal_setup)
+    purge_urls(
+        portal_setup,
+        [
+            "//++theme++ruddocom/less/theme-compiled.css",
+        ],
+    )
